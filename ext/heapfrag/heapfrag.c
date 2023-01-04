b99e26e315ea1a11c80a475056210a730d0013a9
Ivan Prisyazhnyy <john.koepi@gmail.com>
2023-01-04 12:57:47 +0100

    impl dep shady counting for 2.7.6

--- ext/heapfrag/heapfrag.c
+++ ext/heapfrag/heapfrag.c
@@ -9,10 +9,15 @@ VALUE sym_pages_total;
 VALUE sym_pages_seen;
 VALUE sym_pages_walive;
 VALUE sym_pages_wdead;
+VALUE sym_pages_wshady;
+VALUE sym_pages_wuncol;
+VALUE sym_pages_wremset;
+VALUE sym_pages_frag_shady;
 VALUE sym_pages_free;
 VALUE sym_pages_full;
 VALUE sym_alive_objs;
 VALUE sym_dead_objs;
+VALUE sym_shady_objs;
 VALUE sym_heap_allocated_pages;
 VALUE sym_pages_fill_cdf;
 
@@ -29,10 +34,15 @@ struct heap_info {
   size_t pages_seen;
   size_t pages_walive;
   size_t pages_wdead;
+  size_t pages_wshady;
+  size_t pages_wuncol;
+  size_t pages_wremset;
+  size_t pages_frag_shady;
   size_t pages_free;
   size_t pages_full;
   size_t alive_objs;
   size_t dead_objs;
+  size_t shady_objs; // write unprotected objects
   size_t cdf[sizeof(cdf_levels) / sizeof(size_t)];
 };
 
@@ -59,6 +69,7 @@ count_objects_i(void *vstart, void *vend, size_t stride, void *data)
   VALUE v = (VALUE)vstart;
   size_t alive = 0;
   size_t dead = 0;
+  size_t shady = 0;
 
   for (; v != (VALUE)vend; v += stride) {
     if (RBASIC(v)->flags) {
@@ -66,7 +77,16 @@ count_objects_i(void *vstart, void *vend, size_t stride, void *data)
     } else {
       dead ++;
     }
+
+#if USE_RGENGC
+      if (RVALUE_WB_UNPROTECTED(v)) {
+        shady++;
+      }
+#endif
   }
+
+  info->pages_wremset += (GET_HEAP_PAGE(vstart)->flags.has_remembered_objects) ? 1 : 0;
+  info->pages_wuncol += (GET_HEAP_PAGE(vstart)->flags.has_uncollectible_shady_objects) ? 1 : 0;
 
   info->pages_seen ++;
   info->alive_objs += alive;
@@ -80,6 +100,11 @@ count_objects_i(void *vstart, void *vend, size_t stride, void *data)
     info->pages_wdead ++;
   } else {
     info->pages_full ++;
+  }
+  if (shady) {
+    info->pages_wshady ++;
+    info->shady_objs += shady;
+    if (alive && dead) info->pages_frag_shady ++;
   }
 
   insert_cdf(info, alive);
@@ -102,10 +127,15 @@ heapfrag_stat(VALUE mod)
   rb_hash_aset(rv, sym_pages_seen, INT2FIX(info.pages_seen));
   rb_hash_aset(rv, sym_pages_walive, INT2FIX(info.pages_walive));
   rb_hash_aset(rv, sym_pages_wdead, INT2FIX(info.pages_wdead));
+  rb_hash_aset(rv, sym_pages_wshady, INT2FIX(info.pages_wshady));
+  rb_hash_aset(rv, sym_pages_wuncol, INT2FIX(info.pages_wuncol));
+  rb_hash_aset(rv, sym_pages_wremset, INT2FIX(info.pages_wremset));
+  rb_hash_aset(rv, sym_pages_frag_shady, INT2FIX(info.pages_frag_shady));
   rb_hash_aset(rv, sym_pages_free, INT2FIX(info.pages_free));
   rb_hash_aset(rv, sym_pages_full, INT2FIX(info.pages_full));
   rb_hash_aset(rv, sym_alive_objs, INT2FIX(info.alive_objs));
   rb_hash_aset(rv, sym_dead_objs, INT2FIX(info.dead_objs));
+  rb_hash_aset(rv, sym_shady_objs, INT2FIX(info.shady_objs));
   rb_hash_aset(rv, sym_pages_fill_cdf,
                rb_ary_new_from_args(6, INT2FIX(info.cdf[0]), INT2FIX(info.cdf[1]), INT2FIX(info.cdf[2]),
                                     INT2FIX(info.cdf[3]), INT2FIX(info.cdf[4]), INT2FIX(info.cdf[5])));
@@ -124,10 +154,15 @@ Init_heapfrag(void)
   sym_pages_seen = ID2SYM(rb_intern("pages_seen"));
   sym_pages_walive = ID2SYM(rb_intern("pages_with_alive"));
   sym_pages_wdead = ID2SYM(rb_intern("pages_with_dead"));
+  sym_pages_wshady = ID2SYM(rb_intern("pages_with_shady"));
+  sym_pages_wuncol = ID2SYM(rb_intern("pages_with_uncol"));
+  sym_pages_wremset = ID2SYM(rb_intern("pages_with_remset"));
+  sym_pages_frag_shady = ID2SYM(rb_intern("fragmented_with_shady"));
   sym_pages_free = ID2SYM(rb_intern("pages_free"));
   sym_pages_full = ID2SYM(rb_intern("pages_full"));
   sym_alive_objs = ID2SYM(rb_intern("objs_alive"));
   sym_dead_objs = ID2SYM(rb_intern("objs_dead"));
+  sym_shady_objs = ID2SYM(rb_intern("objs_shady"));
   sym_heap_allocated_pages = ID2SYM(rb_intern("heap_allocated_pages"));
   sym_pages_fill_cdf = ID2SYM(rb_intern("heap_pages_fill_cdf"));
 }
--- ext/heapfrag/heapfrag.h
+++ ext/heapfrag/heapfrag.h
@@ -13,6 +13,13 @@ size_t rb_gc_stat(VALUE hash_or_sym);
 /* default tiny heap size: 16KB */
 #define HEAP_PAGE_ALIGN_LOG 14
 #endif
+#define CEILDIV(i, mod) (((i) + (mod) - 1)/(mod))
+
+typedef uintptr_t bits_t;
+enum {
+    BITS_SIZE = sizeof(bits_t),
+    BITS_BITLENGTH = ( BITS_SIZE * CHAR_BIT )
+};
 
 enum {
   HEAP_PAGE_ALIGN = (1UL << HEAP_PAGE_ALIGN_LOG),
@@ -21,7 +28,82 @@ enum {
   HEAP_PAGE_SIZE = (HEAP_PAGE_ALIGN - REQUIRED_SIZE_BY_MALLOC),
   HEAP_PAGE_HEADER_SIZE = sizeof(void *),
   RVALUE_SIZE = 40,
-  HEAP_PAGE_OBJ_LIMIT = (unsigned int)((HEAP_PAGE_SIZE - HEAP_PAGE_HEADER_SIZE)/RVALUE_SIZE)
+  HEAP_PAGE_OBJ_LIMIT = (unsigned int)((HEAP_PAGE_SIZE - HEAP_PAGE_HEADER_SIZE)/RVALUE_SIZE),
+  HEAP_PAGE_BITMAP_LIMIT = CEILDIV(CEILDIV(HEAP_PAGE_SIZE, RVALUE_SIZE), BITS_BITLENGTH)
 };
+
+struct list_node
+{
+	struct list_node *next, *prev;
+};
+
+struct heap_page {
+    short total_slots;
+    short free_slots;
+    short pinned_slots;
+    short final_slots;
+    struct {
+	unsigned int before_sweep : 1;
+	unsigned int has_remembered_objects : 1;
+	unsigned int has_uncollectible_shady_objects : 1;
+	unsigned int in_tomb : 1;
+    } flags;
+
+    struct heap_page *free_next;
+    void *start;
+    void *freelist;
+    struct list_node page_node;
+
+#if USE_RGENGC
+    bits_t wb_unprotected_bits[HEAP_PAGE_BITMAP_LIMIT];
+#endif
+    /* the following three bitmaps are cleared at the beginning of full GC */
+    bits_t mark_bits[HEAP_PAGE_BITMAP_LIMIT];
+#if USE_RGENGC
+    bits_t uncollectible_bits[HEAP_PAGE_BITMAP_LIMIT];
+    bits_t marking_bits[HEAP_PAGE_BITMAP_LIMIT];
+#endif
+
+    /* If set, the object is not movable */
+    bits_t pinned_bits[HEAP_PAGE_BITMAP_LIMIT];
+};
+
+struct heap_page_header {
+    struct heap_page *page;
+};
+
+struct heap_page_body {
+    struct heap_page_header header;
+    /* char gap[];      */
+    /* RVALUE values[]; */
+};
+
+#define GET_PAGE_BODY(x)   ((struct heap_page_body *)((bits_t)(x) & ~(HEAP_PAGE_ALIGN_MASK)))
+#define GET_PAGE_HEADER(x) (&GET_PAGE_BODY(x)->header)
+#define GET_HEAP_PAGE(x)   (GET_PAGE_HEADER(x)->page)
+
+#define NUM_IN_PAGE(p)   (((bits_t)(p) & HEAP_PAGE_ALIGN_MASK)/RVALUE_SIZE)
+#define BITMAP_INDEX(p)  (NUM_IN_PAGE(p) / BITS_BITLENGTH )
+#define BITMAP_OFFSET(p) (NUM_IN_PAGE(p) & (BITS_BITLENGTH-1))
+#define BITMAP_BIT(p)    ((bits_t)1 << BITMAP_OFFSET(p))
+
+/* Bitmap Operations */
+#define MARKED_IN_BITMAP(bits, p)    ((bits)[BITMAP_INDEX(p)] & BITMAP_BIT(p))
+#define MARK_IN_BITMAP(bits, p)      ((bits)[BITMAP_INDEX(p)] = (bits)[BITMAP_INDEX(p)] | BITMAP_BIT(p))
+#define CLEAR_IN_BITMAP(bits, p)     ((bits)[BITMAP_INDEX(p)] = (bits)[BITMAP_INDEX(p)] & ~BITMAP_BIT(p))
+
+#if USE_RGENGC
+#define GET_HEAP_WB_UNPROTECTED_BITS(x) (&GET_HEAP_PAGE(x)->wb_unprotected_bits[0])
+
+#define RVALUE_WB_UNPROTECTED_BITMAP(obj) MARKED_IN_BITMAP(GET_HEAP_WB_UNPROTECTED_BITS(obj), (obj))
+
+#define RVALUE_PAGE_WB_UNPROTECTED(page, obj) MARKED_IN_BITMAP(page->wb_unprotected_bits, (obj))
+
+static inline int
+RVALUE_WB_UNPROTECTED(VALUE obj) {
+  return RVALUE_WB_UNPROTECTED_BITMAP(obj) != 0;
+}
+#endif
+
 
 #endif
